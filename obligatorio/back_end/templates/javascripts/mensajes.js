12cb1f19693b0d3308f3bc9689364cf31e1d0b78
ManuViola77 <manuela.viola@gmail.com>
2016-11-03 23:13:14 -0300

    Herencia de template - creación del padre y heredado para lugares.

---
+++ obligatorio/back_end/templates/javascripts/mensajes.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+  $(document).ready(function(){
+    {%if not rows%}
+      toastr.info('No se encontraron resultados para la busqueda "{{buscar}}"','info');
+    {%endif%}
+    {%if messages%}
+      {%for message in messages%}
+        {#tags viene de la vista#}
+        {%comment%}
+        o
+        varias lines
+        {%endcomment%}
+        toastr.{{message.tags}}("{{message}}")
+      {%endfor%}
+    {%endif%}
+  });
+</script>
